a5e2af2a1fc6cbd7918fd4e11b626bcd2a938426
eads72 <96619838+eads72@users.noreply.github.com>
2022-09-08 11:40:08 +0200

    fixed the bug in pointer

--- 01/exercise.c
+++ 01/exercise.c
@@ -15,13 +15,16 @@ int main(){
    int buffer_length;
    printf("Type the legnth of the buffer: \n");
    scanf("%d", &buffer_length);
+   //printf("%d", buffer_length);
    // unsigned char theString[buffer_length];
-   unsigned char* string_buf = (unsigned char*) malloc(buffer_length);
+   unsigned char* string_buf = (unsigned char*) malloc(sizeof(unsigned char) * buffer_length);
    printf("Type a string: \n");
    scanf("%s", string_buf);
    //*theString = string_buf;
-   printf("your pointer is : %p \n", string_buf);
-   printf("your pointer as a number is: %hhn \n", string_buf);
+   printf("your pointer is : %p \n", (void *) string_buf);
+
+   unsigned long long int ptr_as_int = (unsigned long long int) string_buf;
+   printf("your pointer as a number is: %llu \n", ptr_as_int);
    printf("your string is: %s \n", string_buf);
    printf("length of the string is: %d \n", get_length(string_buf));
    printf("buffer size: %lu\n", sizeof(string_buf));
