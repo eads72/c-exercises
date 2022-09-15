e7f1e2525d67c5bc3ecd1263c09bb2ede07fc3a9
eads72 <96619838+eads72@users.noreply.github.com>
2022-09-15 12:23:30 +0200

    add element function

--- 02/dynamic-array.c
+++ 02/dynamic-array.c
@@ -21,9 +21,7 @@ struct DynArray{
 // returns the struct of the array
 struct DynArray create_array(){
     struct DynArray array;
-    int* a;
-    a = malloc(sizeof(int)*INIT_LEN);
-    array.beginning_address = a;
+    array.beginning_address = (int*) malloc(sizeof(int) * INIT_LEN);
     array.buffer_length = INIT_LEN;
     array.number_of_elements = 0;
     return array;
@@ -31,15 +29,45 @@ struct DynArray create_array(){
 
 void print_array(struct DynArray array){
     printf("Buffer length is: %d \n", array.buffer_length);
+    printf("Number of elements is: %d \n", array.number_of_elements);
+    printf("[ ");
+    for(int i = 0; i < array.number_of_elements; i++){
+        printf("%d ", array.beginning_address[i]);
+    }
+    printf("]\n");
 }
 
-//adds elements to the end of the array
-//takes the struct of the array and the new element as a parameter
-void add_single_element(struct DynArray* array, int new_element){
+// Adds elements to the end of the array
+// Takes the struct of the array and the new element as a parameter
+//
+// Returns 1 if the operation is successful, 0 otherwise
+unsigned char add_single_element(struct DynArray* array, int new_element){
+    if(array->buffer_length <= array->number_of_elements){
+        unsigned int new_buffer_size = 2 * array->buffer_length;
+        unsigned int new_realloc_size = new_buffer_size * sizeof(int);
 
+        int* new_array = (int*) realloc(array->beginning_address, new_realloc_size);
+
+        if (new_array == 0){
+            return 0;
+        }
+        
+        array->buffer_length = new_buffer_size;
+        array->beginning_address = new_array;
+    }
+
+    array->beginning_address[array->number_of_elements] = new_element;
+    array->number_of_elements++;
+
+    return 1;
 }
 
 int main(){
     struct DynArray myArray = create_array();
+    print_array(myArray);
+
+    for(int i = 0; i < 100; i++) {
+        add_single_element(&myArray, i * i);
+    }
     print_array(myArray);
 }
