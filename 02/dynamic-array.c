c8c5d567ac866d51039926334daf690db65ba8d2
eads72 <96619838+eads72@users.noreply.github.com>
2022-09-15 18:30:01 +0200

    insert, remove, reverse, and get functions

--- 02/dynamic-array.c
+++ 02/dynamic-array.c
@@ -62,12 +62,78 @@ unsigned char add_single_element(struct DynArray* array, int new_element){
     return 1;
 }
 
+// First paramter is the pointer to the structure 
+// Takes the index as the second parameter, and removes that value in the index.
+// Returns the removed value
+int remove_element(struct DynArray* array, int index_of_element_to_be_removed){
+    
+    int removed_num = array->beginning_address[index_of_element_to_be_removed];
+
+    for( int i = index_of_element_to_be_removed; i < array->number_of_elements; i++){
+        array->beginning_address[i] = array->beginning_address[i+1];
+    }
+
+    array->number_of_elements--;
+    return  removed_num;
+}
+
+
+// Inserts the new value to the desired index
+void insert_element(struct DynArray* array, int target_index, int new_value){
+
+    add_single_element(array, 0);
+
+    for( int i = array->number_of_elements-1; i > target_index ; i--){
+        array->beginning_address[i] = array->beginning_address[i-1];
+    }
+
+    array->beginning_address[target_index]= new_value;
+}
+
+
+// Reverses the elements in the array
+void reverse(struct DynArray* array){
+    for(int i = 0; i < array->number_of_elements/2; i++){
+        int from_start = array->beginning_address[i];
+        int from_end = array->beginning_address[array->number_of_elements-i-1];
+        array->beginning_address[i] = from_end;
+        array->beginning_address[array->number_of_elements-i-1] = from_start;
+    }
+}
+
+
+
+
+int get(struct DynArray* array, int what_you_want){
+
+    if(what_you_want < array->number_of_elements && what_you_want >= 0){
+        return array->beginning_address[what_you_want];
+    }
+    
+    printf("Invalid index: %d, size is %d. Terminating.\n", what_you_want, array->number_of_elements);
+    exit(1);
+
+}
+
+
 int main(){
+
     struct DynArray myArray = create_array();
     print_array(myArray);
+    int removed_num;
 
-    for(int i = 0; i < 100; i++) {
+    for(int i = 0; i < 12; i++) {
         add_single_element(&myArray, i * i);
     }
     print_array(myArray);
+    removed_num = remove_element(&myArray, 3);
+    printf("Removed number is: %d\n", removed_num);
+    print_array(myArray);
+
+    insert_element(&myArray, 2, 3);
+    print_array(myArray);
+    reverse(&myArray);
+    print_array(myArray);
+    printf("the index 3 is: %d\n", get(&myArray, 3));
+    get(&myArray, -6);
 }
